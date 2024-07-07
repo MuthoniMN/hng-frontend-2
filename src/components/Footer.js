74454533b4733f435351a7180b5f4fcc5963ed17
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-07-07 14:14:34 +0300

    feat: footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,32 @@
+import Image from "next/image";
+
+const Footer = () => {
+    return (
+        <footer className="space-y-8 p-12 h-1/3 text-white">
+            <Image src={"/imgs/light-logo.png"} alt="Furhome E-commerce Website" width={150} height={50}  />
+            <section className="flex justify-between flex-wrap md:flex-nowrap">
+                <div className="w-1/4 space-y-2">
+                    <p>About Us</p>
+                    <p>Blog</p>
+                    <p>FAQ</p>
+                </div>
+                <div className="w-1/4 space-y-2">
+                    <p>Home</p>
+                    <p>Search products</p>
+                    <p>Categories</p>
+                    <p>Wish List</p>
+                    <p>Cart</p>
+                </div>
+                <div className="min-w-1/4 space-y-2">
+                    <h3 className="text-xl">Contact Us</h3>
+                    <p>support@furhome.com</p>
+                    <p>1-800-123-4567</p>
+                    <h4 className="text-lg">Customer Support</h4>
+                    <p>Have questions or need assistance?<br />Our support team is here to help</p>
+                </div>
+            </section>
+        </footer>
+    )
+}
+
+export default Footer;
