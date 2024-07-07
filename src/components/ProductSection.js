9186591ceda573a49b1dc83e04441638a52a21a2
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-07-07 14:15:51 +0300

    feat: product section component

---
+++ src/components/ProductSection.js
@@ -0,0 +1,16 @@
+import products from "@/data/products"
+import ProductCard from "./ProductCard"
+
+const ProductSection = () => {
+    return (
+        <section className="flex flex-wrap justify-around gap-6 p-12">
+            {
+                products.map(prod => (
+                    <ProductCard product={prod} key={prod.title} />
+                ))
+            }
+        </section>
+    )
+}
+
+export default ProductSection;
