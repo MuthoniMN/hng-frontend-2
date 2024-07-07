4ae2d2ea04e54ed9b3c3be37962f1261db0712f4
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-07-07 11:05:35 +0300

    feat: product list

--- src/data/products.js
+++ src/data/products.js
@@ -1,10 +1,66 @@
 const products = [
     {
-        title: "",
-        description: "",
-        price: "",
-        discount: "",
-        picture: ""
+        title: "Blazer Sofa",
+        description: "A comfy emerald green 2-seater sofa",
+        price: "$1200",
+        discount: "30%",
+        picture: "/imgs/blazer-sofa.png"
+    },
+    {
+        title: "Mesh table",
+        description: "A round mesh table for your living room",
+        price: "$400",
+        discount: "20%",
+        picture: "/imgs/mesh-table.png"
+    },
+    {
+        title: "Sleek seater",
+        description: "A red leather outdoor chair",
+        price: "$250",
+        discount: "15%",
+        picture: "/imgs/sleek-seater.png"
+    },
+    {
+        title: "Bar Chair",
+        description: "Bar stools for your kitchen island",
+        price: "$150",
+        discount: "35%",
+        picture: "/imgs/bar-chair.png"
+    },
+    {
+        title: "Pose Chair",
+        description: "A white seat that will add flavor to your home",
+        price: "$200",
+        discount: "20%",
+        picture: "/imgs/pose-chair.png"
+    },
+    {
+        title: "Cushy Sofa",
+        description: "A comfy brown leather 2-seater sofa",
+        price: "$1400",
+        discount: "30%",
+        picture: "/imgs/cushy-sofa.png"
+    },
+    {
+        title: "Baby Seater",
+        description: "A comfy blue 1-seater couch",
+        price: "$350",
+        discount: "10%",
+        picture: "/imgs/baby-seater.png"
+    },
+    {
+        title: "Luxury Sofa",
+        description: "A comfy black leather 2-seater sofa",
+        price: "$2400",
+        discount: "30%",
+        picture: "/imgs/luxury-sofa.png"
+    },
+    {
+        title: "Date Table",
+        description: "A white table",
+        price: "$800",
+        discount: "20%",
+        picture: "/imgs/date-table.png"
     }
 ]
 
