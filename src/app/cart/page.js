a9a72c3bf2f2c6be50e82a34752c23a816adda06
Michelle Ndiangui <105012834+MuthoniMN@users.noreply.github.com>
2024-07-09 13:05:17 +0000

    Feat: completed cart page

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -2,12 +2,15 @@ import PageHeader from "../components/PageHeader";
 import CartItems from "../components/CartItems";
 import Terms from "../components/Terms";
 import PaymentOptions from "../components/PaymentOptions";
+import CartSummary from "../components/CartSummary";
+
 const Cart = () => {
   return (
     <main>
      <PageHeader check={true} title="Shopping Cart" />
       <CartItems />
       <section className="flex flex-col md:flex-row justify-between">
+        <CartSummary />
       <PaymentOptions />
       </section>
       <Terms />
