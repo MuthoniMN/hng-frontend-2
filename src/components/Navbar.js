944360f4225b40fed0733245a1f947999df78764
Michelle Ndiangui <105012834+MuthoniMN@users.noreply.github.com>
2024-07-09 04:53:16 +0000

    Refact: responsive nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,20 +2,24 @@
 import Image from "next/image";
 import SearchBar from "./SearchBar";
 import CategoriesDropdown from "./CategoriesDropdown";
-import { FiShoppingCart } from "react-icons/fi";
 import { FaRegHeart} from "react-icons/fa"
-
+import { FaCartShopping, FaBars } from "react-icons/fa6";
+import { useRouter } from "next/navigation";
 const NavBar = () => {
+    const router = useRouter();
     return (
         <nav className="flex justify-around items-center">
             <Image src={'/imgs/logo.png'} alt="Furhome E-commerce Store" width={150} height={75} />
             <SearchBar />
             <CategoriesDropdown />
-            <p className="flex gap-6 text-xl">
+            <p className="hidden md:flex gap-6 text-xl">
                 <FaRegHeart />
-                <FiShoppingCart />
+                <FaCartShopping onClick={() => router.push('/cart')} />
             </p>
-            <Image src={'/imgs/user-profile.png'} alt="Furhome E-commerce Store" width={56} height={56} className="w-[56px] h-[56px] rounded-full" />
+            <div className="w-[56px] h-[56px]">
+                        <Image src={'/imgs/user-profile.png'} alt="Furhome E-commerce Store" width={56} height={56} className="w-[100%] rounded-full" />
+                        </div>
+            <FaBars className="text-3xl block md:hidden"  />
         </nav>
     )
 }
