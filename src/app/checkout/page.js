e20d62f2da7c8f1365fb95642dc898d4cf0cb881
Michelle Ndiangui <105012834+MuthoniMN@users.noreply.github.com>
2024-07-09 13:32:41 +0000

    Feat: complete checkout page

---
+++ src/app/checkout/page.js
@@ -0,0 +1,20 @@
+import PageHeader from "../components/PageHeader";
+import CheckoutItems from "../components/CheckoutItems";
+import Terms from "../components/Terms";
+import PaymentOptions from "../components/PaymentOptions";
+import CheckoutForm from "../components/CheckoutForm";
+const Checkout = () => {
+  return (
+    <main>
+     <PageHeader check={false} title="Checkout Items" />
+      <CheckoutItems />
+      <section className="flex flex-col md:flex-row justify-between py-6">
+        <CheckoutForm />
+      <PaymentOptions />
+      </section>
+      <Terms />
+    </main>
+  )
+}
+
+export default Checkout
