426d0c3c32372945a276c71d6401260c33010684
Michelle Ndiangui <105012834+MuthoniMN@users.noreply.github.com>
2024-07-09 13:02:46 +0000

    Feat: cart summary component

---
+++ src/components/CartSummary.js
@@ -0,0 +1,26 @@
+const CartSummary = () => {
+  return (
+    <div className="flex flex-col gap-4 py-4 px-2 w-full md:w-3/4 bg-white">
+    <div className="flex justify-between">
+    <h4 className="text-xl">Summary</h4>
+      <p>2 × 1</p>
+    </div>
+      <div className="flex justify-between">
+    <p className="text-xl">Subtotal</p>
+        <p>$2000</p>
+      </div>
+            <div className="flex justify-between">
+    <p className="text-xl">Shipping Fee</p>
+        <p>$450</p>
+      </div>
+            <div className="flex justify-between">
+    <p className="text-xl">Saved</p>
+        <p>$50</p>
+      </div>
+            <div className="flex justify-between">
+    <p className="text-xl">Total</p>
+        <p>$2450</p>
+      </div>
+      <button className="bg-emerald-500 text-white px-2 py-2 rounded-md">Checkout</button>
+    </div>)
+}
