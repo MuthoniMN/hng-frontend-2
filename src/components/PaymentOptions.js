453ac45dba4b05ac2b052fd0d2818bb0a8e666da
Michelle Ndiangui <105012834+MuthoniMN@users.noreply.github.com>
2024-07-09 12:23:52 +0000

    Feat: payment options

---
+++ src/components/PaymentOptions.js
@@ -0,0 +1,16 @@
+import { FaCcVisa, FaCcMastercard, FaPaypal } from "react-icons/fa6";
+
+const PaymentOptions = () => {
+  return (
+    <div className="flex flex-col gap-4">
+      <h1 className="text-2xl">Pay With</h1>
+      <div className="flex md:flex-col lg:flex-row justify-center">
+        <FaCcVisa className="text-emerald-500 text-xl" />
+        <FaCcMastercard className="text-emerald-500 text-xl" />
+        <FaPaypal className="text-emerald-500 text-xl" />
+      </div>
+    </div>
+  )
+}
+
+export default PaymentOptions
