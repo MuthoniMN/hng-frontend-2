ab27bc0863ac2ff9d4e7edf27892dab6b5138c7f
Michelle Ndiangui <105012834+MuthoniMN@users.noreply.github.com>
2024-07-09 05:54:16 +0000

    Refact: fixing mobile sizing

--- src/components/ProductSection.js
+++ src/components/ProductSection.js
@@ -3,7 +3,7 @@ import ProductCard from "./ProductCard"
 
 const ProductSection = () => {
     return (
-        <section className="flex flex-wrap justify-around gap-6 p-12">
+        <section className="flex flex-wrap justify-around gap-6 p-4 lg:p-12">
             {
                 products.map(prod => (
                     <ProductCard product={prod} key={prod.title} />
