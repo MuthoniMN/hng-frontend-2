0bb6f33fc11a5d55eccbfab0f30550e6be8d83c8
Michelle Ndiangui <105012834+MuthoniMN@users.noreply.github.com>
2024-07-09 12:24:37 +0000

    Refact: adding payment and t&c

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -1,11 +1,18 @@
 import PageHeader from "../components/PageHeader";
 import CartItems from "../components/CartItems";
-
+import Terms from "../components/Terms";
+import PaymentOptions from "../components/PaymentOptions";
 const Cart = () => {
   return (
     <main>
      <PageHeader check={true} title="Shopping Cart" />
       <CartItems />
+      <section className="flex flex-col md:flex-row justify-between">
+      <PaymentOptions />
+      </section>
+      <Terms />
     </main>
   )
 }
+
+export default Cart
