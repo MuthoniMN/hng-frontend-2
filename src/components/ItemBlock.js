338fecc61baf6aa333cfaf806f85234194f6c0fd
Michelle Ndiangui <105012834+MuthoniMN@users.noreply.github.com>
2024-07-09 10:16:37 +0000

    Feat: item block component

---
+++ src/components/ItemBlock.js
@@ -0,0 +1,43 @@
+import { FaCircleCheck, FaEdit } from "react-icons/fa6";
+
+const ItemBlock = ({ item, cart}) => {
+  return (
+    <div className="flex justify-around p-4">
+      <FaCircleCheck className="text-emerald-500 text-xl" />
+      <Image src={item.picture} alt={item.name} width={250} height 
+        ={250} className="w-2/5" />
+      <div className="flex flex-col gap-2">
+      <h3 className="text-lg">{item.name}</h3>
+        <p className="text-2xl">{item.price}</p>
+        <p className="bg-gray-100 px-4 py-2 rounded">{item.discount} off</p>
+        {
+          !cart && <div className="flex gap-2 items-center self-end">
+          <button className="bg-gray-100 w-[24px] h-[24px] rounded-full flex items-center justify-center">+</button>
+            <p className="text-lg">1</p>
+                      <button className="bg-gray-100 w-[24px] h-[24px] rounded-full flex items-center justify-center">-</button>
+                      
+          </div>
+        }
+      </div>
+
+              {
+          cart && <div className="flex gap-2 items-center">
+          <button className="bg-gray-100 w-[24px] h-[24px] rounded-full flex items-center justify-center">+</button>
+            <p className="text-lg">1</p>
+                      <button className="bg-gray-100 w-[24px] h-[24px] rounded-full flex items-center justify-center">-</button>
+                      
+          </div>
+        }
+
+      {
+        !cart && <div className="flex gap-2 items-center">
+          <FaEdit />
+          <p>Edit item</p>
+</div>
+
+      } 
+    </div>
+  )
+}
+
+export default ItemBlock;
