575fc12d779e01cc408b267ec1a65b1f23f2e770
Michelle Ndiangui <105012834+MuthoniMN@users.noreply.github.com>
2024-07-09 13:03:53 +0000

    Refact: styling changes

--- src/components/PaymentOptions.js
+++ src/components/PaymentOptions.js
@@ -2,7 +2,7 @@ import { FaCcVisa, FaCcMastercard, FaPaypal } from "react-icons/fa6";
 
 const PaymentOptions = () => {
   return (
-    <div className="flex flex-col gap-4">
+    <div className="flex flex-col gap-4 bg-white px-4 py-2">
       <h1 className="text-2xl">Pay With</h1>
       <div className="flex md:flex-col lg:flex-row justify-center">
         <FaCcVisa className="text-emerald-500 text-xl" />
