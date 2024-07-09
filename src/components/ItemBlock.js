891e7c0859bdba04b67da9975247191f32ec3b92
Michelle Ndiangui <105012834+MuthoniMN@users.noreply.github.com>
2024-07-09 16:52:28 +0000

    Bug: adding import + styling changes

--- src/components/ItemBlock.js
+++ src/components/ItemBlock.js
@@ -1,13 +1,14 @@
-import { FaCircleCheck, FaEdit } from "react-icons/fa6";
-
+import { FaCircleCheck} from "react-icons/fa6";
+import Image from 'next/image'
+import {FaEdit} from "react-icons/fa"
 const ItemBlock = ({ item, cart}) => {
   return (
-    <div className="flex justify-around p-4">
+    <div className="flex justify-around p-4 max-w-[640px]">
       <FaCircleCheck className="text-emerald-500 text-xl" />
       <Image src={item.picture} alt={item.name} width={250} height 
         ={250} className="w-2/5" />
       <div className="flex flex-col gap-2">
-      <h3 className="text-lg">{item.name}</h3>
+      <h3 className="text-lg">{item.title}</h3>
         <p className="text-2xl">{item.price}</p>
         <p className="bg-gray-100 px-4 py-2 rounded">{item.discount} off</p>
         {
