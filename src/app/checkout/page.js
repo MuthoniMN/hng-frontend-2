17ce7dd0688a0a73c4009fe65a3a0d86b23eb1c6
Michelle Ndiangui <105012834+MuthoniMN@users.noreply.github.com>
2024-07-10 12:22:01 +0300

    spacing

--- src/app/checkout/page.js
+++ src/app/checkout/page.js
@@ -8,7 +8,7 @@ const Checkout = () => {
     <main className="space-y-6">
      <PageHeader check={false} title="Checkout Items" />
       <CheckoutItems />
-      <section className="flex flex-col md:flex-row justify-between">
+      <section className="flex flex-col md:flex-row justify-between gap-6">
         <section className="space-y-4 bg-gray-100 py-4">
         <h2 className="text-2xl">Enter Card Information</h2>
           <CheckoutForm />
