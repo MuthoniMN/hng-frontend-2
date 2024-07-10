81c2ef85db7e6655a7cbe57a360a1bbd2033b0ac
Michelle Ndiangui <105012834+MuthoniMN@users.noreply.github.com>
2024-07-10 18:37:25 +0300

    payment sizing

--- src/components/PaymentOptions.js
+++ src/components/PaymentOptions.js
@@ -2,7 +2,7 @@ import { FaCcVisa, FaCcMastercard, FaPaypal } from "react-icons/fa6";
 
 const PaymentOptions = () => {
   return (
-    <div className="flex flex-col gap-4 bg-white w-full md:1/4 lg:2/5 px-4 py-2 rounded">
+    <div className="flex flex-col gap-4 bg-white w-full md:w-1/4 lg:w-w/5 px-4 py-2 rounded">
       <h1 className="text-2xl">Pay With</h1>
       <div className="flex md:flex-col lg:flex-row gap-4 justify-center">
         <FaCcVisa className="text-3xl" />
